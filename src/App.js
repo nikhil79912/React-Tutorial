c76f3b9ed14fc9b821113238d1707f5849e88f97
nikhil <nikhilgkp97@gmail.com>
2023-02-01 16:36:54 +0530

    componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ class App extends React.Component{
   constructor(){
     super()
     this.state={
-      name:"nikk"
+      count:0
     }
     console.warn('constructor')
   }
-  componentDidMount(){
-    console.warn("componetn did mount")
+  componentDidUpdate(preProps,preState,snAPshot){
+    console.warn("componetn did Update" ,preState.count,this.state.count)
+    if(this.state.count<10){
+      this.setState({count:this.state.count+1})
+      // alert("data is already same")
+    }
   }
   render(){
     console.warn("render")
     return (
       <div className='app'>
-       <h1>Component Did Mount {this.state.name}</h1>
-       <button onClick={()=>this.setState({name:"thor"})}>update name</button>
+       <h1>Component Did update {this.state.count} </h1>
+      <button onClick={()=>{this.setState({count:1})}}>updadate name</button>
       </div>
     )
   }
