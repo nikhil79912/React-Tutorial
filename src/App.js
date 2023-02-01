2a61c6e4b3cb642bf01300e9a3643f2586831d31
nikhil <nikhilgkp97@gmail.com>
2023-02-01 16:05:59 +0530

    component Did Mount

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import User from './component/user'
-
-
-
-const App=()=>{
-  const[name,setname]=React.useState("nik")
-  return (
-    <div className='app'>
-      <h1>Render Method In React </h1>
-      <User name={name}/>
-      <button onClick={()=>setname("nikka")}>update Name</button>
-    </div>
-  )
+// import User from './component/user'
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      name:"nikk"
+    }
+    console.warn('constructor')
+  }
+  componentDidMount(){
+    console.warn("componetn did mount")
+  }
+  render(){
+    console.warn("render")
+    return (
+      <div className='app'>
+       <h1>Component Did Mount {this.state.name}</h1>
+       <button onClick={()=>this.setState({name:"thor"})}>update name</button>
+      </div>
+    )
+  }
+  
+  
 }
+
+
+
+  
+
 
 export default App;
